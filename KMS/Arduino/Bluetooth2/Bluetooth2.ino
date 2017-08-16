4d7491621e1aeab9f23e6a55bb52495eb37453cc
tony9402 <tony9402@naver.com>
2017-08-16 10:06:31 +0900

    Arduino

--- KMS/Arduino/Bluetooth2/Bluetooth2.ino
+++ KMS/Arduino/Bluetooth2/Bluetooth2.ino
@@ -6,6 +6,7 @@
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 SoftwareSerial BTSerial(2,3);
+int num = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -31,16 +32,24 @@ void loop() {
   
   String content= "";
   byte letter;
+  byte Num = 0;
   content.toUpperCase();
+  num = (int)mfrc522.uid.size;
+  Serial.print(num);
+  Serial.write(num);
   for(byte i=0;i<mfrc522.uid.size;i++){
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+     if(Serial.available()){
+  BTSerial.print(HEX);
+  }
   }
   content.toUpperCase( );
 
-  if(Serial.available()){
-  BTSerial.write(mfrc522.uid.uidByte,mfrc522.uid.size);
-  }
+
+    Serial.println();
+    
+  
 }
---
+++ KMS/Arduino/RFID_PLUS_BLUETOOTH/RFID_PLUS_BLUETOOTH.ino
@@ -0,0 +1,40 @@
+#include<SoftwareSerial.h>
+#include<MFRC522.h>
+#include<SPI.h>
+
+#define Rx 2
+#define Tx 3
+#define RST_PIN 9
+#define SS_PIN 10
+
+SoftwareSerial BT(Rx,Tx);
+MFRC522 mfrc(SS_PIN,RST_PIN);
+
+unsigned int _4ByteNum = 0;
+
+void setup()
+{
+ Serial.begin(9600);
+ BT.begin(9600);
+ SPI.begin();
+ mfrc.PCD_Init(); 
+}
+
+void loop()
+{
+    _4ByteNum = 0;
+    if((mfrc.PICC_IsNewCardPresent()) && (mfrc.PICC_ReadCardSerial()))
+    {
+      for(int Count = 0; Count < mfrc.uid.size;Count++){
+        _4ByteNum = _4ByteNum | ((mfrc.uid.uidByte[Count] & 0xff) << 24 - 8 * Count);
+        delay(1);
+      }
+      Serial.print(_4ByteNum,HEX);
+      Serial.println();
+    }
+    if(_4ByteNum != 0){
+      BT.write(mfrc.uid.uidByte,4);
+    }
+    delay(2000);
+}
+
