efc609ed710d222b749650c18572831df3ae0458
mjcart <30867895+mjcart@users.noreply.github.com>
2017-08-27 15:31:37 +0900

    dc motor
    check

---
+++ JMJ/dc__driver.ino
@@ -0,0 +1,32 @@
+
+void setup() {
+  pinMode(7,OUTPUT);
+ pinMode(9,OUTPUT);
+ pinMode(11,OUTPUT);
+pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(11,HIGH);
+digitalWrite(13,HIGH);
+digitalWrite(7,LOW);
+digitalWrite(9,LOW);
+delay(1000);
+digitalWrite(11,HIGH);
+digitalWrite(13,HIGH);
+digitalWrite(7,LOW);
+digitalWrite(9,HIGH);
+delay(1000);
+digitalWrite(11,HIGH);
+digitalWrite(13,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(9,LOW);
+delay(1000);
+digitalWrite(11,HIGH);
+digitalWrite(13,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(9,HIGH);
+delay(1000);
+}
