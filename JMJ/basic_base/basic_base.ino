a321d26db1996074493873bff05f1fe460004791
mjcart <30867895+mjcart@users.noreply.github.com>
2017-09-16 02:04:40 +0900

    d

--- JMJ/basic_base/basic_base.ino
+++ JMJ/basic_base/basic_base.ino
@@ -36,3 +36,70 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+void disCheck(){
+  float durationI, durationR, durationL;
+  digitalWrite(trig_pin,LOW);
+  
+  digitalWrite(echoI_pin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin,LOW);
+  durationI = pulseIn(echoI_pin,HIGH,60000UL);
+  distanceI = durationI / 2.0 / 29.0;
+  Serial.print("I : ");
+  Serial.print(distanceI);
+  Serial.println();
+  
+  digitalWrite(echoR_pin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin,LOW);
+  durationR = pulseIn(echoR_pin, HIGH,60000UL);
+  distanceR = durationR / 2.0 / 29.0;
+  Serial.print("R : ");
+  Serial.print(distanceR);
+  Serial.println();
+
+  digitalWrite(echoL_pin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin,LOW);
+  durationL = pulseIn(echoL_pin, HIGH,60000UL);
+  distanceL = durationL / 2.0 / 29.0;
+  Serial.print("L : ");
+  Serial.print(distanceL);
+  Serial.println();
+  delay(1000);
+  return;
+}
+void Stop(){
+  digitalWrite(Ldc,HIGH);
+  digitalWrite(Rdc,HIGH);
+  return;
+}
+
+void straight(){
+  analogWrite(Ldc,150);
+  analogWrite(Rdc,150);
+  digitalWrite(Lturn,LOW);
+  digitalWrite(Rturn,LOW);
+  return;
+}
+
+void turnLeft(){
+  digitalWrite(Ldc,LOW);
+  digitalWrite(Rdc,LOW);
+  digitalWrite(Lturn,HIGH);
+  digitalWrite(Rturn,LOW);
+  delay(350);
+  digitalWrite(Ldc,LOW);
+  digitalWrite(Rdc,LOW);
+  digitalWrite(Lturn,LOW);
+  digitalWrite(Rturn,HIGH);
+  delay(50);
+  Stop();
+  return;
+}
